3810624a7ef065dc917497ddc0080b539cb6c28a
aberry5621 <aberry5621@gmail.com>
2017-04-19 17:42:41 -0700

    Initial Commit

---
+++ Ants V Doodlebugs Simulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ants V Doodlebugs Simulation
+//
+//  Created by ax on 4/19/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
